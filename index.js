8a230e892c2bc4a2caeaf4ddd75aa77e013b8576
hariom <hariomsh.jpr@gmail.com>
2025-03-14 12:30:07 +0530

    add push pop mathods in function for stack DSA

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-// function expression
-let product = function (x, y) {
-  return x * y;
-};
+// // function expression
+// let product = (x, y) => {
+//   console.log(y);
+//   return x * y;
+// };
 
-result = product(5, 10);
+// result = product(5);
 
-console.log(result); // 50
+// console.log(result); // 50
 
-// function declaration
-function sum(x, y) {
-  return x + y;
+// // function declaration
+// function sum(x, y) {
+//   return x + y;
+// }
+
+// result = sum(5, 10);
+// console.log(result); // 15
+
+let data = [];
+let currentsize = data.length;
+
+let max = 5;
+
+function push(newelement) {
+  if (currentsize >= max) {
+    console.log("Stack Overflow");
+  }
+  if (currentsize < max) {
+    data[currentsize] = newelement;
+    currentsize = currentsize + 1;
+    console.log(data);
+  }
 }
 
-result = sum(5, 10);
-console.log(result); // 15
+function pop() {
+  if (currentsize <= 0) {
+    console.log("Stack Underflow");
+  }
+  if (currentsize > 0) {
+    currentsize = currentsize - 1;
+    console.log(data[currentsize]);
+    console.log(data);
+  }
+}
 
+push(1);
+push(2);
+push(3);
+
+pop();
