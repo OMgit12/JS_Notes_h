204589423926a951f79308f44e771e88600855d0
hariom <hariomsh.jpr@gmail.com>
2025-03-15 23:28:11 +0530

    add New chnages

--- index.js
+++ index.js
@@ -17,6 +17,41 @@ num = 5;
 const numbr = 5;
 
 // assign 10 to num
-numbr = 10;
+// numbr = 10;
 console.log(numbr); // Error! constant cannot be changed
 // Always Initialize a Constant During Declaration
+
+// JavaScript Symbol
+// A symbol is a unique and immutable data type that is used as an identifier for object properties.
+// two symbols with the same description
+let value1 = Symbol(true);
+let value2 = Symbol(true);
+
+console.log(value1 === value2); // false
+// console.log(4 ** 2);
+
+console.log(4 !== "4");
+
+// JavaScript Type Conversion
+// type conversion is the process of converting data of one type to another. For example, converting "string" data to number.
+// There are two types of type conversion in JavaScript:
+// Implicit Conversion - Automatic type conversion.
+// Explicit Conversion - Manual type conversion.
+
+// numeric string used with + gives string type
+let result;
+
+// convert number to string
+result = "3" + 2;
+console.log(result, "-", typeof result);
+
+result = "3" + true;
+console.log(result, "-", typeof result);
+
+result = "3" + null;
+console.log(result, "-", typeof result);
+
+// js mei jb bhi apn kisi string ko number mei convert krna hota hai to use parseInt() function use krte hain.
+let str = "123";
+let num1 = parseInt(str);
+console.log(num1);
