9f37bdeb0da9e087b2ec7d6d6e49f5fa829a4815
hariom <hariomsh.jpr@gmail.com>
2025-04-24 00:25:46 +0530

    add asynchronous js

--- es6.js
+++ es6.js
@@ -203,7 +203,7 @@ for (let i of string) {
 // setTimeout(function, milliseconds);  syntax
 
 let afhfhd = setTimeout(() => {
-    console.log('Hello');
+    // console.log('Hello');
 }, 3000); // output Hello after 3 seconds
 
 // console.log(afhfhd); // output 1
@@ -226,6 +226,187 @@ greet('Peter', callMe); // in this callback = callMe and then we call the callba
 
 // In the above program, there are two functions. While calling the greet() function, two arguments (a string value and a function) are passed.
 // The callMe() function is a callback function.
-
 // The benefit of using a callback function is that you can wait for the result of a previous function call and then execute another function call.
+
+
+// Using a Callback Function
+// However, if you want to wait for the result of the previous function call before the next statement is executed, you can use a callback function. For example,
+
+// Callback Function Example
+function greet(name, myFunction) { // in whixh myFunction is equal to sayName() function then we call the myFunction() function inside the greet() function. 
+    console.log('Hello world');
+
+    // callback function
+    // executed only after the greet() is executed
+    myFunction(name);
+}
+
+// callback function
+function sayName(name) {
+    console.log('Hello' + ' ' + name);
+}
+
+// calling the function after 2 seconds
+setTimeout(greet, 2000, 'John', sayName); // in this case, the greet() function is called after 2 seconds and the sayName() function is passed as a callback function to the greet() function. The greet() function is executed first and then the sayName() function is executed after that.
+
+
+
+
+// JavaScript Promise and Promise Chaining
+// In JavaScript, a promise is a good way to handle asynchronous operations.It is used to find out if the asynchronous operation is successfully completed or not.
+// A promise may have one of three states.
+
+// Pending
+// Fulfilled
+// Rejected
+
+// A promise starts in a pending state.That means the process is not complete.If the operation is successful, the process ends in a fulfilled state.And, if an error occurs, the process ends in a rejected state.
+// For example, when you request data from the server by using a promise, it will be in a pending state.When the data arrives successfully, it will be in a fulfilled state.If an error occurs, then it will be in a rejected state.
+
+// Create a Promise
+// To create a promise object, we use the Promise() constructor.
+let promise = new Promise(function (resolve, reject) {
+    //do something
+});
+// The Promise() constructor takes a function as an argument.The function also accepts two functions resolve() and reject().
+// If the promise returns successfully, the resolve() function is called.And, if an error occurs, the reject() function is called.
+
+
+// Program with a Promise
+const count = true;
+
+let countValue = new Promise(function (resolve, reject) { // resolve, reject are callback functions that are passed as arguments to the Promise() constructor. 
+    if (count) {
+        resolve("There is a count value.");
+    } else {
+        reject("There is no count value");
+    }
+});
+
+console.log(countValue);  // output: Promise {<resolved>: "There is a count value."}
+// In the above program, a Promise object is created that takes two functions: resolve() and reject(). resolve() is used if the process is successful and reject() is used when an error occurs in the promise.
+
+// JavaScript Promise Chaining
+// Promises are useful when you have to handle more than one asynchronous task, one after another. For that, we use promise chaining.
+// You can perform an operation after a promise is resolved using methods then(), catch() and finally().
+
+// JavaScript then() method
+// The then() method is used with the callback when the promise is successfully fulfilled or resolved.
+// promiseObject.then(onFulfilled, onRejected);  : Syntax
+
+
+// returns a promise
+
+let countValue1 = new Promise(function (resolve, reject) {
+    resolve("Promise resolved");
+});
+
+// executes when promise is resolved successfully
+
+countValue1
+    .then(function successValue(result) {
+        console.log(result);
+    })
+
+    .then(function successValue1() {
+        console.log("You can call multiple functions this way.");
+    });
+
+// In the above program, the then() method is used to chain the functions to the promise. The then() method is called when the promise is resolved successfully.
+// You can chain multiple then() methods with the promise.
+
+// JavaScript catch() method
+// The catch() method is used with the callback when the promise is rejected or if an error occurs. For example,
+// returns a promise
+let countValue2 = new Promise(function (resolve, reject) {
+    reject('Promise rejected');
+});
+
+// executes when promise is resolved successfully
+countValue2.then(
+    function successValue(result) {
+        console.log(result);
+    },
+)
+    // executes if there is an error
+    .catch(
+        function errorValue(result) {
+            console.log(result);
+        }
+    );
+
+// In the above program, the promise is rejected. And the catch() method is used with a promise to handle the error.
+
+
+
+
+// JavaScript async/await
+// We use the async keyword with a function to represent that the function is an asynchronous function. The async function returns a promise.
+async function name(parameter1, parameter2, ...paramaterN) {
+    // statements
+}
+// name - name of the function
+// parameters - parameters that are passed to the function
+
+async function f() {
+    console.log('Async function.');
+    return Promise.resolve(1);
+}
+
+f().then(function (result) {
+    console.log(result)
+});
+// Since this function returns a promise, you can use the chaining method then() like this:
+
+// JavaScript await Keyword
+// The await keyword is used inside the async function to wait for the asynchronous operation.
+// The use of await pauses the async function until the promise returns a result(resolve or reject) value.For example,
+
+// a promise
+let promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('Promise resolved')
+    }, 4000);
+});
+
+// async function
+async function asyncFunc() {
+    // wait until the promise resolves 
+    let result = await promise1;
+
+    console.log(result);
+    console.log('hello');
+}
+
+// calling the async function
+asyncFunc();
+// In the above program, a Promise object is created and it gets resolved after 4000 milliseconds.Here, the asyncFunc() function is written using the async function.
+
+
+// Error Handling in Async/Await
+// While using the async function, you write the code in a synchronous manner.And you can also use the catch () method to catch the error.For example,
+// a promise
+let promise5 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        resolve('Promise resolved')
+    }, 4000);
+});
+
+// async function
+async function asyncFunc() {
+    try {
+        // wait until the promise resolves 
+        let result = await promise5;
+        console.log(result);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+// calling the async function
+asyncFunc(); // Promise resolved
+
+
+
+
 
